68325beca1d069022e48a99cfc3506dd9783ee80
czambanini <ca.zambanini@gmail.com>
2023-12-17 11:52:23 -0300

    Adição da classe Mesas

---
+++ SistemaGestaoRestaurante/StatusMesa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestaoRestaurante
+{
+    enum StatusMesa
+    {
+        Livre = 1,
+        Reservada = 2,
+        AguardandoAtendimento = 3,
+        AguardandoPrato = 4,
+        Ocupada = 5,
+    }
+}
