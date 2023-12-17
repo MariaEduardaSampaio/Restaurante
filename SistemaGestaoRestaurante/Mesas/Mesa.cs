338a7d98395dd0c368d96fc17653809006a3a840
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2023-12-17 15:56:10 -0300

    adiciona métodos de exibir mesas conforme status

--- SistemaGestaoRestaurante/Mesas/Mesa.cs
+++ SistemaGestaoRestaurante/Mesas/Mesa.cs
@@ -2,13 +2,13 @@
 
 namespace SistemaGestaoRestaurante.Mesas
 {
-    internal class Mesa
+    internal static class Mesa
     {
       
-        Dictionary<int, StatusMesa> Mesas = new Dictionary<int, StatusMesa>()
+        public static Dictionary<int, StatusMesa> Mesas = new Dictionary<int, StatusMesa>()
         {
             { 01, StatusMesa.Livre },
-            { 02, StatusMesa.AguardandoPrato },
+            { 02, StatusMesa.Livre },
             { 03, StatusMesa.Livre },
             { 04, StatusMesa.Livre },
             { 05, StatusMesa.Livre },
@@ -21,33 +21,33 @@ namespace SistemaGestaoRestaurante.Mesas
             { 12, StatusMesa.Livre },
         };
 
-        public void ReservarMesa(int idMesa)
+        public static void ReservarMesa(int idMesa)
         {
             Mesas[idMesa] = StatusMesa.Reservada;
         }
 
-        public void SolicitarAtendimento(int idMesa)
+        public static void SolicitarAtendimento(int idMesa)
         {
             Mesas[idMesa] = StatusMesa.AguardandoAtendimento;
             Console.WriteLine($"Mesa {idMesa} chamou o garçom");
         }
 
-        public void AguardarPedido(int idMesa)
+        public static void AguardarPedido(int idMesa)
         {
             Mesas[idMesa] = StatusMesa.AguardandoPrato;
         }
 
-        public void MesaOcupada(int idMesa)
+        public static void MesaOcupada(int idMesa)
         {
             Mesas[idMesa] = StatusMesa.Ocupada;
         }
 
-        public void LiberarMesa(int idMesa)
+        public static void LiberarMesa(int idMesa)
         {
             Mesas[idMesa] = StatusMesa.Livre;
         }
 
-        public void ExibirMesasAguardandoAtendimento()
+        public static void ExibirMesasAguardandoAtendimento()
         {
             Console.WriteLine("Mesas aguardando atendimento:");
             foreach (KeyValuePair<int, StatusMesa> mesa in Mesas)
@@ -62,22 +62,56 @@ namespace SistemaGestaoRestaurante.Mesas
                 Console.WriteLine("Sem mesas aguardando atendimento.");
         }
 
-        public void ExibirMesasAguardandoPrato()
+        public static void ExibirMesasAguardandoPrato()
         {
             Console.WriteLine("Mesas aguardando pedido:");
             foreach (KeyValuePair<int, StatusMesa> mesa in Mesas)
             {
                 if (mesa.Value == StatusMesa.AguardandoPrato)
-                {
                     Console.WriteLine($"Mesa: {mesa.Key}");
-                }
             }
 
             if (!Mesas.ContainsValue(StatusMesa.AguardandoPrato))
                 Console.WriteLine("Fila livre.");
         }
 
+        public static void ExibirMesasOcupadas()
+        {
+            Console.WriteLine("Mesas ocupadas:");
+            foreach (KeyValuePair<int, StatusMesa> mesa in Mesas)
+            {
+                if (mesa.Value == StatusMesa.Ocupada)
+                    Console.WriteLine($"Mesa: {mesa.Key}");
+            }
 
+            if (!Mesas.ContainsValue(StatusMesa.Ocupada))
+                Console.WriteLine("Nenhuma mesa ocupada.");
+        }
 
+        public static void ExibirMesasLivres()
+        {
+            Console.WriteLine("Mesas livres:");
+            foreach (KeyValuePair<int, StatusMesa> mesa in Mesas)
+            {
+                if (mesa.Value == StatusMesa.Livre)
+                    Console.WriteLine($"Mesa: {mesa.Key}");
+            }
+
+            if (!Mesas.ContainsValue(StatusMesa.Livre))
+                Console.WriteLine("Nenhuma mesa livre.");
+        }
+
+        public static void ExibirMesasReservadas()
+        {
+            Console.WriteLine("Mesas reservadas:");
+            foreach (KeyValuePair<int, StatusMesa> mesa in Mesas)
+            {
+                if (mesa.Value == StatusMesa.Reservada)
+                    Console.WriteLine($"Mesa: {mesa.Key}");
+            }
+
+            if (!Mesas.ContainsValue(StatusMesa.Reservada))
+                Console.WriteLine("Fila livre.");
+        }
     }
 }
